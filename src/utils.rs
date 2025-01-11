76365b0f84a0bd7f9ca7cb0cca01c7d70904f0a6
t4mpz <tampinha1527@gmail.com>
2025-01-11 12:59:04 -0300

    Added base working utils for the env file

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,11 @@
 pub mod utils{
   use std::fs;
-  use dotenvy::dotenv;
+  use dotenvy;
 
-  
+  pub fn write_base_env(file_name: String){
+    let contents = "SCRAPS_FOLDER=scraps\nDEFAULT_FOLDER=downloads";
+    let _ = fs::write(file_name, contents);
+  }
+
+  pub fn getEnv(key: String) -> String{ return dotenvy::var(key).unwrap_or("".to_string()); }
 }
