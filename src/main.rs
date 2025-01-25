441c9b8f15feac4b75fb66dd84e24a4cfe870307
t4mpz <tampinha1527@gmail.com>
2025-01-25 10:29:24 -0300

    feat/dev: Added internal arguments for tests

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,16 @@ use utils::utils::{fix_title_to_path, get_env};
 use pdfer::pdfer::{download_images, mesh_scraps};
 use scrapper::scrapper::{fetch_chapters, fetch_images, retrieve_body, fetch_image_results};
 
+fn gen_using_args() -> Vec<String> {
+  if get_env("MODE") != "debug" { return args().collect(); }
+  else{
+    let mut internal_args: Vec<String> = Vec::new();
+    internal_args.push("mddf".to_string()); // has to have this first arg so it corresponds to the system
+    internal_args.push("q".to_string());
+    return internal_args;
+  }
+}
+
 fn download_manga_chapter(chapter_url: &String){
   let borrowed_url = chapter_url.to_string();
   let pages = fetch_images(retrieve_body(borrowed_url).unwrap());
@@ -24,7 +34,7 @@ fn search_manga_chapter(search_terms: &String) {
 }
 
 fn main() {
-  let argc: Vec<String> = args().collect();
+  let argc: Vec<String> = gen_using_args();
   let dft_val = "".to_string();
   // TODO add args size verification
   let main_arg = argc.get(1).unwrap_or(&dft_val);
