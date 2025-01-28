64c3467b72d3d79ee9ae9f50959036e1cb17e6b4
t4mpz <tampinha1527@gmail.com>
2025-01-28 10:55:54 -0300

    dev: Started working on screen CLI Feedback

---
+++ src/argscommands.rs
@@ -0,0 +1,29 @@
+pub mod argscommands {
+  use crate::scrapper::scrapper;
+  use crate::utils::utils;
+  use crate::pdfer::pdfer;
+
+  pub fn download_manga_chapter(chapter_url: &String, chapter_title: &String){
+    let borrowed_url = chapter_url.to_string();
+    let borrowed_title = utils::fix_title_to_path(chapter_title.to_string());
+    let pages = scrapper::fetch_images(scrapper::retrieve_body(borrowed_url).unwrap());
+    let downloaded_pages = pdfer::download_images(pages);
+    let _ = pdfer::mesh_scraps(downloaded_pages, borrowed_title); // TODO create url to parsable file title
+  }
+  
+  pub fn search_manga_chapter(search_terms: &String) {
+    let bd = scrapper::retrieve_body(
+      format!("{}/{}", utils::get_env("DEFAULT_SEARCH_LINK"), utils::fix_title_to_path(search_terms.to_string()))
+    ).unwrap();
+    let results = scrapper::fetch_image_results(bd);
+    results.iter().for_each(|f | println!("{}", f.img_href));
+  }
+
+  pub fn download_story(story_url: &String) {
+    let chapters = scrapper::fetch_chapters(scrapper::retrieve_body(story_url.to_string()).unwrap());
+    chapters.iter().for_each(|cpt| {
+      download_manga_chapter(&cpt.href, &cpt.title);
+    });
+  }
+  
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod utils;
+mod argscommands;
 mod scrapper;
 mod structures;
 mod pdfer;
+mod screens;
 use std::env::args;
-use utils::utils::{fix_title_to_path, get_env};
-use pdfer::pdfer::{download_images, mesh_scraps};
-use scrapper::scrapper::{fetch_chapters, fetch_images, retrieve_body, fetch_image_results};
+use utils::utils::get_env;
 
 fn gen_using_args() -> Vec<String> {
   if get_env("MODE") != "debug" { return args().collect(); }
@@ -17,32 +17,20 @@ fn gen_using_args() -> Vec<String> {
   }
 }
 
-fn download_manga_chapter(chapter_url: &String){
-  let borrowed_url = chapter_url.to_string();
-  let pages = fetch_images(retrieve_body(borrowed_url).unwrap());
-  let downloaded_pages = download_images(pages);
-  println!("Downloaded images");
-  let _ = mesh_scraps(downloaded_pages, "teste".to_string()); // TODO create url to parsable file title
-}
-
-fn search_manga_chapter(search_terms: &String) {
-  let bd = retrieve_body(
-    format!("{}/{}", get_env("DEFAULT_SEARCH_LINK"), fix_title_to_path(search_terms.to_string()))
-  ).unwrap();
-  let results = fetch_image_results(bd);
-  results.iter().for_each(|f | println!("{}", f.img_href));
+fn args_command_selector(argc: Vec<String>) {
+  let dft_val = "".to_string();
+  let main_arg = argc.get(1).unwrap_or(&dft_val);
+  if *main_arg == "dc".to_string() {
+    println!("recong the arg");
+    argscommands::argscommands::download_manga_chapter(argc.get(2).unwrap(), argc.get(3).unwrap());
+  }
+  else if *main_arg == "q".to_string() {
+    argscommands::argscommands::search_manga_chapter(&"yotsuba".to_string());
+  }
 }
 
 fn main() {
   let argc: Vec<String> = gen_using_args();
-  let dft_val = "".to_string();
   // TODO add args size verification
-  let main_arg = argc.get(1).unwrap_or(&dft_val);
-  if *main_arg == "dc".to_string() {
-    println!("recong the arg");
-    download_manga_chapter(argc.get(2).unwrap());
-  }
-  else if *main_arg == "q".to_string() {
-    search_manga_chapter(&"yotsuba".to_string());
-  }
+  args_command_selector(argc);
 }
