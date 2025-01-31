4be862b424c7b542e361f5706023825b1c6da8ed
t4mpz <tampinha1527@gmail.com>
2025-01-31 19:17:42 -0300

    cargo: removed cli table, I'll exibit the results and text as plain

--- src/main.rs
+++ src/main.rs
@@ -36,5 +36,5 @@ fn main() {
   // TODO add args size verification
   // args_command_selector(argc);
   let results = argscommands::argscommands::search_manga_chapter(&"yotsuba".to_string());
-  let toc = screens::screens::result_renderer(results, Some(true));
+  println!("{}", screens::screens::result_renderer(results, Some(true)));
 }
--- src/structures.rs
+++ src/structures.rs
@@ -20,18 +20,4 @@ pub mod structures{
     pub last_chapter: String,
     pub additional_info: Vec<String>
   }
-
-  #[derive(Table)]
-  pub struct SearchResultTable { 
-    #[table(title="href", justify="Justify::Center")]
-    pub href: String,
-    #[table(title="Image", justify="Justify::Center")]
-    pub img_href: String,
-    #[table(title="Name", justify="Justify::Center")]
-    pub story_name: String,
-    #[table(title="Last Chapter", justify="Justify::Center")]
-    pub last_chapter: String,
-    #[table(title="Additional Info", justify="Justify::Center")]
-    pub additional_info: String
-  }
 }
